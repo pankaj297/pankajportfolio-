5134f3a2758d959f550d5fb60d2f705102fc075d
pankajnaik <115211948+pankaj297@users.noreply.github.com>
2024-06-10 19:00:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+
+const mobile_nav = document.querySelector(".mobile-nav-btn");
+const nav_header = document.querySelector(".header-section");
+
+const toggleNavbar = () => {
+  // alert("Hello javaScript");
+  nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
+
+
+
+
+
+
+function myFunction(){
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
